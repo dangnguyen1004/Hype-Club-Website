be3501fd4aea89880d10cdfba55cc001079fc9ba
Bui Huu Dang <buihuudang2000@gmail.com>
2020-12-08 08:51:40 +0700

    login.php

--- newsample/header3.php
+++ newsample/header3.php
@@ -1,6 +1,6 @@
             <div>
             <a href="" class="bag"><img src="../newsample/images/icon/bag.png" alt=""></a>
-            <a href="../manager/index.php" class="user"><img src="../newsample/images/icon/user.png" alt=""></a>
+            <a href="login.php" class="user"><img src="../newsample/images/icon/user.png" alt=""></a>
             <a href="javascript:void(0)" class="ham-burger">
                 <span></span>
                 <span></span>
